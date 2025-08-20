5976625244647bcf30736a2148c184c3fa4c9b82
Arijit Datta from Kolkata <arijitdatta.kolkata@gmail.com>
2025-08-20 08:51:23 +0530

    initial checkin

---
+++ Day02_DataTypes.py
@@ -0,0 +1,15 @@
+# How to comment out a line  Control + /
+
+x=5.5;
+y=10;
+print(x+y); #Statement 1
+x=-1;
+print (x);
+x='hello hello'
+y='wow'
+print(x)
+print(y)
+x,y,z=1, "22s",55.5 # Important structure
+
+print("Prints", x, ":",y,":",z) # Statement 2- + does not work - if + is used like java - it simply concatenates i.e. it treats x as a string if it has a preceding string. Compare with Statement1 
+
---
+++ Day03_ControlStructures.py
@@ -0,0 +1,14 @@
+a = 7
+b = 10
+
+if a < b:
+    print("hello")
+else:
+    print("world")
+
+if a < 5:
+    print("less than 5")
+elif a == 5:
+    print("equal to 5") 
+else:
+    print("greater than 5") 
